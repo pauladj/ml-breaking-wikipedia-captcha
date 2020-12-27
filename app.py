f11424499280373bf78189b2faa24512858f2541
Paula <pauladejaime@gmail.com>
2020-12-27 15:23:11 +0100

    Deleted page title from app

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from test_model import import_model
 from test_model import predict_captcha_image
 
 title = "Breaking Wikipedia's captcha 🔠"
-st.beta_set_page_config(page_title=title)
 st.title(title)
 
 st.markdown('<style>{}</style>'.format(""".block-container { 
